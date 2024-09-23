3b4db5c3eeb401e27a3e7b15a27145b7e5bc02f9
flex-57 <129852644+flex-57@users.noreply.github.com>
2024-09-23 11:44:06 +0200

    Fin TriDeNombre

--- 01 - Algorithmes/TriDeNombre/Program.cs
+++ 01 - Algorithmes/TriDeNombre/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+
+Console.WriteLine("Veuillez entrer un premier nombre :");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Veuillez entrer un deuxième nombre :");
+int B = int.Parse(Console.ReadLine());
+Console.WriteLine("Veuillez entrer un troisième nombre :");
+int C = int.Parse(Console.ReadLine());
+
+int[] arrayNum = new int[]
+{
+    A, B, C
+};
+
+Array.Sort(arrayNum);
+
+foreach (int i in arrayNum)
+{
+    Console.WriteLine(i);
+}
+Console.ReadLine();
