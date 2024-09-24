a45838080b543027bd048c2d4dfd10ddde0a5e65
flex-57 <129852644+flex-57@users.noreply.github.com>
2024-09-24 22:02:31 +0200

    Ajout du controle de saisie limitée

---
+++ 01 - Algorithmes/LesBoucles/ControllerLaSaisieAvecLimite/Program.cs
@@ -0,0 +1,40 @@
+﻿const string PASSWORD = "formation";
+const int MAX_ATTEMPS = 3;
+
+string passwordVerif;
+int remainingAttemps;
+int counter = 1;
+bool isOk;
+
+do
+{
+    remainingAttemps = MAX_ATTEMPS - counter;
+
+    Console.WriteLine("Veuillez taper le mot de passe :");
+    passwordVerif = Console.ReadLine();
+
+    if(passwordVerif.Equals(PASSWORD)) {
+        Console.WriteLine(Environment.NewLine + "Vous êtes connecté !");
+        isOk = true;
+    }
+
+    else
+    {
+        if(remainingAttemps == 0)
+        {
+            Console.WriteLine(Environment.NewLine + "Votre compte est bloqué !");
+        }
+
+        else
+        {
+            Console.WriteLine(Environment.NewLine + "Mot de passe incorrect! Il vous reste " + remainingAttemps + " tentatives...");
+        }
+
+        Console.WriteLine(" ");
+        counter++;
+        isOk = false;
+    }
+
+    
+}
+while (remainingAttemps > 0 && !isOk);
