5fc47fa3499b18ebb131c6c55761b699909f351a
flex-57 <129852644+flex-57@users.noreply.github.com>
2024-09-26 09:15:10 +0200

    Creation du répertoire LesTableaux +Ajout de Rechercher un nombre dans un tableau

---
+++ 01 - Algorithmes/LesTableaux/RechercheNombreDansTableau/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] numbers = [8, 16, 32, 64, 128, 256, 512];
+int number = 0;
+bool isOk = false;
+
+while(!isOk)
+{
+    Console.WriteLine("Veuillez entrer un nombre :");
+    isOk = int.TryParse(Console.ReadLine(), out number);
+}
+
+int index = Array.FindIndex(numbers, n => n.Equals(number));
+
+if(index != -1)
+{
+    Console.WriteLine($"Valeur de l'indice correspondant à {number} : {index}");
+}
+
+else
+{
+    Console.WriteLine("Nombre non trouvé !");
+}
