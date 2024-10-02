da7ae92f4f4831ad5515b97fc40be22cd8f37268
flex-57 <129852644+flex-57@users.noreply.github.com>
2024-10-03 01:25:25 +0200

    Remplacement de readline par readkey dans FabTools  + modification des checks par une regex dans controller la           saisie, es tu majeur et cherche solution pour les fruits et legumes

--- 01 - Algorithmes/LesConditions/EsTuMajeur/Program.cs
+++ 01 - Algorithmes/LesConditions/EsTuMajeur/Program.cs
@@ -1,16 +1,53 @@
-﻿Console.WriteLine("Quel est votre age ?");
-int age = int.Parse(Console.ReadLine());
+﻿using FabriceTools;
+using System.Text.RegularExpressions;
 
-Console.WriteLine("Réponse :");
-if (age > 0)
+class Program
 {
-    if (age < 18) Console.WriteLine("Vous êtes mineur !");
-    else Console.WriteLine("Vous êtes majeur !");
-}
-else
-{
-    Console.WriteLine("Vous n'êtes pas encore né !");
-}
+    static void Main()
+    {
+        do
+        {
+            Console.Write("Veuillez entrer votre âge : ");
+            string input = Console.ReadLine();
 
-Console.WriteLine("\nAppuyez sur entrée pour continuer.");
-Console.ReadLine();
+            try
+            {
+                // Vérifie si l'entrée correspond à une expression régulière pour un nombre entier
+                if (Regex.IsMatch(input, @"^(-?\d+)$"))
+                {
+                    int age = int.Parse(input);
+
+                    if (age <= 0)
+                    {
+                        Console.WriteLine("Vous n'êtes pas encore né(e) !");
+                    }
+                    else if (age < 18)
+                    {
+                        Console.WriteLine("Vous êtes mineur(e).");
+                    }
+                    else if (age < 130)
+                    {
+                        Console.WriteLine("Vous êtes majeur(e).");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Vous êtes mort(e) !");
+                    }
+                }
+                else
+                {
+                    throw new FormatException("Format incorrect : veuillez entrer un âge valide.");
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erreur : {ex.Message}");
+            }
+        }
+        while (ConsolePrompt.TryAgain());
+    }
+}
