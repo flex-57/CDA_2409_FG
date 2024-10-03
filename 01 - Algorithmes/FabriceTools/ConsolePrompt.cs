8a46f71f6203514650286fce3c1760aebf63f528
flex-57 <129852644+flex-57@users.noreply.github.com>
2024-10-03 15:41:52 +0200

    Ajout de la methode AskPositiveFloat et AskInteger dans FabriceTools

--- 01 - Algorithmes/FabriceTools/ConsolePrompt.cs
+++ 01 - Algorithmes/FabriceTools/ConsolePrompt.cs
@@ -12,5 +12,50 @@
             // Vérifie si l'utilisateur a appuyé sur 'o' ou 'n'
             return key.KeyChar == 'o' || key.KeyChar == 'O';
         }
+
+        public static int AskInteger(string _question)
+        {
+            string prompt;
+            int returnValue;
+            bool isOk;
+
+            do
+            {
+                Console.WriteLine(_question);
+                prompt = Console.ReadLine() ?? "";
+
+                isOk = int.TryParse(prompt, out returnValue);
+
+                if (!isOk)
+                {
+                    Console.WriteLine("Saisie invalide, recommencez !");
+                }
+            }
+            while (!isOk);
+
+            return returnValue;
+        }
+        public static float AskPositiveFloat(string _question)
+        {
+            string prompt;
+            float returnValue;
+            bool isOk;
+
+            do
+            {
+                Console.WriteLine(_question);
+                prompt = Console.ReadLine() ?? "";
+
+                isOk = float.TryParse(prompt, out returnValue) && returnValue >= 0;
+
+                if (!isOk)
+                {
+                    Console.WriteLine("Saisie invalide, recommencez !");
+                }
+            }
+            while (!isOk);
+
+            return returnValue;
+        }
     }
 }
