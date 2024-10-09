81a05c69ae57facbf6ae4241a5435ac44dc55c51
flex-57 <129852644+flex-57@users.noreply.github.com>
2024-10-09 21:44:26 +0200

    Modification IsValid CB

--- 01 - Algorithmes/Algorithmes/ValidCréditCard/isValid.cs
+++ 01 - Algorithmes/Algorithmes/ValidCréditCard/isValid.cs
@@ -4,75 +4,56 @@ namespace Validations
 {
     public class IsValid
     {
-        static string prompt;
-        static bool isOk;
+        static string prompt = "";
 
         public static void ValidCreditCard()
         {
-            do
+            Console.WriteLine("Entrez un numéro de CB : ");
+            prompt = Console.ReadLine() ?? "";
+
+            if (!RegexCheck.creditCard(prompt) && CBCheck.algoLuhn(prompt))
             {
-                Console.WriteLine("Entrez un numéro de CB : ");
-                prompt = Console.ReadLine() ?? "";
-
-                isOk = RegexCheck.creditCard(prompt) && CBCheck.algoLuhn(prompt);
-
-                if (!isOk)
-                {
-                    Console.WriteLine($"\"{prompt}\" n'est pas un numéro de carte de crédit valide !");
-                }
-
-                else
-                {
-                    Console.WriteLine("Numéro de carte de crédit valide, nous espérons que celle-ci est pleine !");
-                }
+                Console.WriteLine($"\"{prompt}\" n'est pas un numéro de carte de crédit valide !");
             }
-            while (!isOk);
+
+            else
+            {
+                Console.WriteLine("Numéro de carte de crédit valide, nous espérons que celle-ci est pleine !");
+            }
         }
 
         public static void ValidTelephoneNumber()
         {
-            do
+            Console.WriteLine("Entrez votre numéro de téléphone :");
+            prompt = Console.ReadLine() ?? "";
+
+            if (!RegexCheck.telephoneNumber(prompt))
             {
-                Console.WriteLine("Entrez votre numéro de téléphone :");
-                prompt = Console.ReadLine() ?? "";
-
-                isOk = RegexCheck.telephoneNumber(prompt);
-
-                if (!isOk)
-                {
-                    Console.WriteLine($"\"{prompt}\" n'est pas un numéro de téléphone valide !");
-                }
-
-                else
-                {
-                    Console.WriteLine("C'est bon!");
-                }
+                Console.WriteLine($"\"{prompt}\" n'est pas un numéro de téléphone valide !");
             }
-            while (!isOk);
+
+            else
+            {
+                Console.WriteLine("C'est bon!");
+            }
         }
 
         public static void ValidPassword()
         {
-            do
+            Console.WriteLine("Entrez un mot de passe : ");
+            Console.WriteLine("(12 caractères minimum, au moins une minuscule, une majuscule, un chiffre et un caractère spécial ou ");
+            Console.WriteLine("20 caractères minimum, au moins une minuscule, une majuscule et un chiffre)");
+            prompt = Console.ReadLine() ?? "";
+
+            if (!RegexCheck.password(prompt))
             {
-                Console.WriteLine("Entrez un mot de passe : ");
-                Console.WriteLine("(12 caractères minimum, au moins une minuscule, une majuscule, un chiffre et un caractère spécial ou ");
-                Console.WriteLine("20 caractères minimum, au moins une minuscule, une majuscule et un chiffre)");
-                prompt = Console.ReadLine() ?? "";
-
-                isOk = RegexCheck.password(prompt);
-
-                if (!isOk)
-                {
-                    Console.WriteLine($"Le mot de passe \"{prompt}\" n'est pas valide !");
-                }
-
-                else
-                {
-                    Console.WriteLine("C'est bon!");
-                }
+                Console.WriteLine($"Le mot de passe \"{prompt}\" n'est pas valide !");
             }
-            while (!isOk);
+
+            else
+            {
+                Console.WriteLine("C'est bon!");
+            }
         }
     }
 }
