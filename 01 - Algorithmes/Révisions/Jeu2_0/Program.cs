cefb53e8cac5245373a92899ebe3f7636d0785d9
flex-57 <129852644+flex-57@users.noreply.github.com>
2024-10-10 09:36:39 +0200

    Mise à jour Jeu du 2-0

--- 01 - Algorithmes/Révisions/Jeu2_0/Program.cs
+++ 01 - Algorithmes/Révisions/Jeu2_0/Program.cs
@@ -8,37 +8,71 @@ namespace Jeu2_0
         {
             do
             {
-                int computeurN = new Random().Next(3);
-                int playerN;
-                int computeurScore = 0;
+                int computerScore = 0;
                 int playerScore = 0;
-                bool isOk;
+                bool stop = false;
 
                 do
                 {
-                    Console.WriteLine("Veuillez choisir un nombre parmi 0, 1 ou 2 (nombre négatif pour terminer) :");
-                    isOk = int.TryParse(Console.ReadLine(), out playerN) && playerN <= 2;
+                    int computerN = new Random().Next(3);
+                    int playerN;
+                    bool isOk;
 
-                    if(!isOk)
+                    do
                     {
-                        Console.WriteLine("Vous devez choisir un nombre entre 0 et 2 !");
-                    }
+                        Console.WriteLine("Veuillez choisir un nombre parmi 0, 1 et 2 (ou un nombre négatif pour terminer) :");
+                        isOk = int.TryParse(Console.ReadLine(), out playerN) && playerN <= 2;
 
-                    else
-                    {
-                        if (playerN >= 0)
+                        if (!isOk)
                         {
-                            Console.Write($"L'ordinateur a choisi le nombre \"{computeurN}\" {Environment.NewLine}");
-
-                            int diff = p
-                            if()
+                            Console.WriteLine("Vous devez choisir un nombre entre 0 et 2 !");
                         }
+
+                        else
+                        {
+                            if (playerN >= 0)
+                            {
+                                Console.Write($"L'ordinateur a choisi le nombre \"{computerN}\"{Environment.NewLine}");
+
+                                Console.WriteLine($"Vous avez choisi le nombre \"{playerN}\"{Environment.NewLine}");
+
+                                string greater = playerN < computerN ? "Ordi" : "Humain";
+
+                                if (Math.Abs(playerN - computerN) == 2)
+                                {
+                                    if (greater == "Ordi")
+                                    {
+                                        computerScore++;
+                                    }
+                                    else
+                                    {
+                                        playerScore++;
+                                    }
+                                }
+
+                                else if (Math.Abs(playerN - computerN) == 1)
+                                {
+                                    if (greater == "Ordi")
+                                    {
+                                        playerScore++;
+                                    }
+                                    else
+                                    {
+                                        computerScore++;
+                                    }
+                                }
+                            }
+
+                            else
+                            {
+                                stop = true;
+                            }
+                        }
+                        Console.WriteLine($"Scores :{Environment.NewLine}Ordi : {computerScore}  |  Humain : {playerScore}{Environment.NewLine}");
                     }
+                    while (!isOk);
                 }
-                while (!isOk);
-
-
-
+                while (computerScore < 10 && playerScore < 10 && !stop);
             }
             while (ConsolePrompt.TryAgain());
         }
