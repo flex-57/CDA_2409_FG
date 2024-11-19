b409dde000596029e17c5ea4c09e58fca57b9c8b
flex-57 <129852644+flex-57@users.noreply.github.com>
2024-11-19 16:59:40 +0100

    jalon start

---
+++ Jalon/energie/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/reset.css">
+    <link rel="stylesheet" href="./styles/main.css">
+    <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet">
+    <title>&Eacute;nergie</title>
+</head>
+<body>
+    <header>
+        <h1 class="brand">Logo</h1>
+        <nav>
+            <label for="hamburger" class="label-hamburger">☰</label>
+            <input type="checkbox" name="hamburger" id="hamburger">
+            <ul>
+                <li><a href="">Accueil</a></li>
+                <!--
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                -->
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section class="accueil">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, eos!
+            </p>
+            <div class="f-wrap">
+                <div class="card">
+                    <img src="https://assets.nicepagecdn.com/d2cc3eaa/2298516/images/227ca9c9.png" alt="">
+                    <span>Lorem ipsum dolor sit.</span>
+                </div>
+                <div class="card">
+                    <img src="https://assets.nicepagecdn.com/d2cc3eaa/2298516/images/5d9f3e6d.png" alt="">
+                    <span>Lorem, ipsum dolor.</span>
+                </div>
+                <div class="card">
+                    <img src="https://assets.nicepagecdn.com/d2cc3eaa/2298516/images/ffe1adcd.png" alt="">
+                    <span>Lorem ipsum dolor sit amet consectetur.</span>
+                </div>
+                <div class="card">
+                    <img src="https://assets.nicepagecdn.com/d2cc3eaa/2298516/images/54feaa5f.png" alt="">
+                    <span>Lorem, ipsum.</span>
+                </div>
+            </div>
+            <button>Obtenez une consultation gratuite</button>
+        </section>
+
+        <section class="page2">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing.
+            </p>
+            <div class="f-wrap">
+                <div class="card">
+                    <img src="https://assets.nicepagecdn.com/d2cc3eaa/2298516/images/227ca9c9.png" alt="">
+                    <span>Lorem ipsum dolor sit.</span>
+                </div>
+                <div class="card">
+                    <img src="https://assets.nicepagecdn.com/d2cc3eaa/2298516/images/5d9f3e6d.png" alt="">
+                    <span>Lorem, ipsum dolor.</span>
+                </div>
+                <div class="card">
+                    <img src="https://assets.nicepagecdn.com/d2cc3eaa/2298516/images/ffe1adcd.png" alt="">
+                    <span>Lorem ipsum dolor sit amet consectetur.</span>
+                </div>
+                <div class="card">
+                    <img src="https://assets.nicepagecdn.com/d2cc3eaa/2298516/images/54feaa5f.png" alt="">
+                    <span>Lorem, ipsum.</span>
+                </div>
+            </div>
+            <button>Obtenez une consultation gratuite</button>
+        </section>
+    </main>
+</body>
+</html>
+
+<!--
+https://website2298516.nicepage.io/fr/Landing.html?version=9c5bd989-8380-4789-9ac2-ebd465c47d93#
+-->
