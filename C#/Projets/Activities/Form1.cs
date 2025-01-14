81a9ae6d61020dcc94b57f86b6e85d048721d6ae
flex-57 <129852644+flex-57@users.noreply.github.com>
2025-01-14 16:21:35 +0100

    modif

--- C#/Projets/Activities/Form1.cs
+++ C#/Projets/Activities/Form1.cs
@@ -10,26 +10,24 @@ using System.Windows.Forms;
 
 namespace Activities
 {
-    public partial class Form1 : Form
+    public partial class InscriptionActivities : Form
     {
-        public Form1()
+        public InscriptionActivities()
         {
             InitializeComponent();
         }
 
         private void BtnRegister_Click(object sender, EventArgs e)
         {
-            ;
+            
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
 
         private void groupBox1_Enter(object sender, EventArgs e)
         {
 
         }
+
+     
     }
 }
