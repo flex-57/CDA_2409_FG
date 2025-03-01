a0157a3f9c4f859a25a4d298d4a3228208ad1c1d
flex-57 <129852644+flex-57@users.noreply.github.com>
2025-03-01 15:40:44 +0100

    petit exercice avec Wafita

--- Javascript/objet/assets/script.js
+++ Javascript/objet/assets/script.js
@@ -9,7 +9,6 @@ const userName = document.querySelector('#user-name')
 
 const tableBloc = document.querySelector('.table-bloc')
 
-userName.innerHTML = `${myEmployee.firstname} ${myEmployee.lastname}`
 
 const rowHead = document.createElement('tr')
 const headLastname = document.createElement('th')
@@ -38,16 +37,41 @@ const columnBirthday = document.createElement('td')
 const columnEmail = document.createElement('td')
 const columnSalary = document.createElement('td')
 
-columnFirstname.innerText = myEmployee.firstname
-columnLastname.innerText = myEmployee.lastname
-columnBirthday.innerText = myEmployee.birthday
-columnEmail.innerText = `${myEmployee.firstname.toLowerCase()}.${myEmployee.lastname.toLowerCase()}@example.com`
-columnSalary.innerHTML = `${myEmployee.salary} €`
+const display = () => {
+    userName.innerHTML = `${myEmployee.firstname} ${myEmployee.lastname}`
+    columnFirstname.innerText = myEmployee.firstname
+    columnLastname.innerText = myEmployee.lastname
+    columnBirthday.innerText = myEmployee.birthday
+    columnEmail.innerText = `${myEmployee.firstname.toLowerCase()}.${myEmployee.lastname.toLowerCase()}@example.com`
+    columnSalary.innerHTML = `${myEmployee.salary} €`
+    
+    tableBloc.append(row)
+    row.append(columnLastname)
+    row.append(columnFirstname)
+    row.append(columnBirthday)
+    row.append(columnEmail)
+    row.append(columnSalary)
+}
 
-tableBloc.append(row)
-row.append(columnLastname)
-row.append(columnFirstname)
-row.append(columnBirthday)
-row.append(columnEmail)
-row.append(columnSalary)
+const inputFirstname = document.querySelector('#input-firstname')
+const inputLastname = document.querySelector('#input-lastname')
+const inputBirthday = document.querySelector('#input-birthday')
+const inputSalary = document.querySelector('#input-salary')
+const btnSave = document.querySelector('#btn-save')
 
+inputFirstname.value = myEmployee.firstname
+inputLastname.value = myEmployee.lastname
+inputBirthday.value = myEmployee.birthday
+inputSalary.value = myEmployee.salary
+
+btnSave.addEventListener('click', () => {
+    myEmployee.firstname = inputFirstname.value
+    myEmployee.lastname = inputLastname.value
+    myEmployee.email = `${myEmployee.firstname.toLowerCase()}.${myEmployee.lastname.toLowerCase()}@example.com`
+    myEmployee.birthday = inputBirthday.value
+    myEmployee.salary = inputSalary.value > myEmployee.salary ? inputSalary.value : myEmployee.salary
+
+    display()
+})
+
+display()
