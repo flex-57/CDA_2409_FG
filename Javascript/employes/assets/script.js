9590dafc6bbcd1aa5247fe5c21862f69691b5bd5
flex-57 <129852644+flex-57@users.noreply.github.com>
2025-03-12 16:58:29 +0100

    upd course + employes

--- Javascript/employes/assets/script.js
+++ Javascript/employes/assets/script.js
@@ -1,6 +1,10 @@
 const tbody = document.querySelector('tbody')
+const tfoot = document.querySelector('tfoot')
+
+const btnSort = document.querySelector('#monthly button')
 
 let resultsData = []
+let sorted = 0
 
 const getResults = async () => {
     try {
@@ -9,25 +13,25 @@ const getResults = async () => {
             throw new Error('Erreur de chargement des données')
         }
         resultsData = (await response.json()).data
-        display()
+        display(resultsData)
     } catch (error) {
         console.error('Erreur', error)
         resultsData = []
     }
 }
 
-const display = () => {
+const display = (result) => {
 
     tbody.innerText = ''
 
-    resultsData.forEach((r) => {
-        const tr = tbody.insertRow()
-        const tdEid = tr.insertCell()
-        const tdName = tr.insertCell()
-        const tdEmail = tr.insertCell()
-        const tdSalary = tr.insertCell()
-        const tdBirth = tr.insertCell()
-        const tdActions = tr.insertCell()
+    result.forEach((r) => {
+        const trBody = tbody.insertRow()
+        const tdEid = trBody.insertCell()
+        const tdName = trBody.insertCell()
+        const tdEmail = trBody.insertCell()
+        const tdSalary = trBody.insertCell()
+        const tdBirth = trBody.insertCell()
+        const tdActions = trBody.insertCell()
 
         const btnDuplicate = document.createElement('button')
         const btnDelete = document.createElement('button')
@@ -41,31 +45,56 @@ const display = () => {
         btnDuplicate.id = r.id
         btnDelete.id = r.id
 
-        console.log(r);
-        
-
         tdEid.innerText = r.id
         tdName.innerText = r.employee_name
         tdEmail.innerText = `${(r.employee_name.split(' ')[0]).charAt(0).toLowerCase()}.${(r.employee_name.split(' ')[1]).toLowerCase()}@email.fr`
         tdSalary.innerText = `${(r.employee_salary / 12).toFixed(2)} €`
         tdBirth.innerText = new Date().getFullYear() - r.employee_age
         tdActions.append(btnDuplicate, btnDelete)
-    })
-    
-    document.querySelectorAll('.btn-duplicate').forEach((btn, idx) => {
-        btn.addEventListener('click', () => {
-            console.log(btn);
+
+        btnDuplicate.addEventListener('click', () => {
+            tfoot.innerText = ''
+            const dup = {
+                id: result.length + 1,
+                employee_name: r.employee_name,
+                employee_salary: r.employee_salary,
+                employee_age: r.employee_age,
+                profile_image: ''
+            }
+            result.push(dup)
+            tfoot.innerText = ''
+            display(result)
+        })
+
+        btnDelete.addEventListener('click', () => {
+            tfoot.innerText = ''
+            display(result.filter(employee => employee.id != btnDelete.id))
         })
     })
+
+    const trFoot = tfoot.insertRow()
+    const tdCount = trFoot.insertCell()
+    const tdVoid = trFoot.insertCell()
+    const tdTotalSalary = trFoot.insertCell()
+    const tdVoid2 = trFoot.insertCell()
+
+    tdVoid.setAttribute('colspan', 2)
+    tdVoid2.setAttribute('colspan', 2)
+
+    tdCount.innerHTML = `<b>${result.length}</b>`
+    tdTotalSalary.innerHTML = `${result.reduce((a, b) => (a + b.employee_salary / 12), 0).toFixed(2)} €`
     
-    document.querySelectorAll('.btn-delete').forEach((btn, idx) => {
-        btn.addEventListener('click', () => {
-            console.log('btn', btn);
-            console.log('idx', idx);
-            
-            resultsData.splice(btn.id - 1, 1)
-            display()
-        })
+    btnSort.addEventListener('click', () => {
+        if(sorted == 0) {
+            tfoot.innerText = ''
+            sorted = 1
+            display(result.sort((a, b) => a.employee_salary - b.employee_salary))
+        }
+        else if(sorted == 1) {
+            tfoot.innerText = ''
+            sorted = 2
+            display(result.sort((a, b) => b.employee_salary - a.employee_salary))
+        }
     })
 }
 
