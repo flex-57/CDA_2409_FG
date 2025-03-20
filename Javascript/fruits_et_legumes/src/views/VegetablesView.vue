a47a6fedb5ea9d31072b1822a9373b291c2024c6
flex-57 <129852644+flex-57@users.noreply.github.com>
2025-03-20 19:58:57 +0100

    j'ai fait de la m....

--- Javascript/fruits_et_legumes/src/views/VegetablesView.vue
+++ Javascript/fruits_et_legumes/src/views/VegetablesView.vue
@@ -1,25 +1,26 @@
 <template>
     <h1>Liste des légumes</h1>
+    {{ storage }}
     <table>
         <thead>
             <tr>
-                <th>Id</th>
                 <th @click="sortVegetables('Name', false)">Nom</th>
                 <th @click="sortVegetables('Variety', false)">Variété</th>
                 <th @click="sortVegetables('PrimaryColor', false)">Couleur</th>
                 <th @click="sortVegetables('LifeTime')">Durée conservation</th>
                 <th @click="sortVegetables('Fresh')">Frais</th>
+                <th @click="sortVegetables('Price')">Price</th>
                 <th>Actions</th>
             </tr>
         </thead>
         <tbody>
             <tr v-for="vegetable in vegetables" :key="vegetable.Id">
-                <td>{{ vegetable.Id }}</td>
                 <td>{{ capitalizeName(vegetable.Name) }}</td>
                 <td>{{ capitalizeName(vegetable.Variety) }}</td>
                 <td>{{ vegetable.PrimaryColor }}</td>
-                <td>{{ vegetable.LifeTime }}</td>
+                <td>{{ vegetable.LifeTime }} day{{ vegetable.LifeTime > 1 ? 's' : '' }}</td>
                 <td>{{ vegetable.Fresh === 0 ? 'Non' : 'Oui' }}</td>
+                <td>{{ vegetable.Price.toFixed(2) }} $</td>
                 <td>
                     <a href="">Éditer</a> -
                     <a href="">Supprimer</a>
@@ -44,12 +45,17 @@ const sortState = ref({
     Fresh: false,
 })
 
+const storage = ref(localStorage.getItem('vegetables'))
+
 const getVegetables = async () => {
+    let fetchedVegetables = []
     try {
-        vegetables.value = await fetchVegetables()
+        fetchedVegetables = await fetchVegetables()
     } catch (e) {
         console.error('Erreur lors du chargement des légumes :', e)
     }
+    const storedVegetables = localStorage.getItem('vegetables')
+    vegetables.value = [...fetchedVegetables, ...storedVegetables]
 }
 
 const sortVegetables = (col, isNum = true) => {
@@ -65,5 +71,7 @@ const sortVegetables = (col, isNum = true) => {
     )
 }
 
-onMounted(getVegetables)
+onMounted(() => {
+     getVegetables()
+})
 </script>
