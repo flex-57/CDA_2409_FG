f8c3ef9a320f56ab19c69dd2d39899a36d5f20a6
flex-57 <129852644+flex-57@users.noreply.github.com>
2025-03-28 11:58:32 +0100

    p4 with vue

---
+++ Javascript/puissance4_vue/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <header>
+        <h1>
+            <span v-for="letter in titleGame" :key="letter">{{ letter }}</span>
+        </h1>
+    </header>
+    <main>
+        <RouterView />
+    </main>
+</template>
+
+<script setup>
+const titleGame = [...'Puissance 4']
+</script>
