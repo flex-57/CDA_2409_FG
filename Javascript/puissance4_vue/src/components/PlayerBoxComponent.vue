97aad55532a259387d834693267ffa81d1d76047
flex-57 <129852644+flex-57@users.noreply.github.com>
2025-03-31 06:38:09 +0200

    ajout component playerbox + objets player + gestion de l'égalité

---
+++ Javascript/puissance4_vue/src/components/PlayerBoxComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <div
+        :id="id"
+        class="player-box"
+        :style="{
+            boxShadow:
+                gameOver && !winner
+                    ? '0 6px 12px #313158'
+                    : currentPlayer.color === player.color
+                      ? `0 6px 12px ${player.color}`
+                      : '0 6px 12px #313158',
+            scale:
+                winner && winner.color === player.color
+                    ? '1.2'
+                    : winner
+                      ? '.8'
+                      : '1',
+        }"
+    >
+        <h2>
+            Joueur {{ player.name.charAt(0).toUpperCase() }}{{ player.name.slice(1).toLowerCase() }}
+        </h2>
+        <div>
+            <h3>Score</h3>
+            <p class="score">{{ player.score }}</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    id: String,
+    player: Object,
+    currentPlayer: Object,
+    winner: Object,
+    gameOver: Boolean,
+})
+</script>
