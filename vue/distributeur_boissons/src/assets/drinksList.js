1c9e178e9d082860deee39058c8768f5e37ebad3
flex-57 <129852644+flex-57@users.noreply.github.com>
2025-04-15 11:59:37 +0200

    bug timeout

---
+++ vue/distributeur_boissons/src/assets/drinksList.js
@@ -0,0 +1,38 @@
+export const drinksList = [
+    {
+        id: 1,
+        name: 'Soda Cola',
+        price: 1.25,
+        stock: 20,
+    },
+    {
+        id: 2,
+        name: 'Eau plate',
+        price: 1.05,
+        stock: 25,
+    },
+    {
+        id: 3,
+        name: 'Eau gazeuse',
+        price: 1.15,
+        stock: 30,
+    },
+    {
+        id: 4,
+        name: "Jus d'orange",
+        price: 1.25,
+        stock: 35,
+    },
+    {
+        id: 5,
+        name: 'Soda Citron',
+        price: 1.25,
+        stock: 0,
+    },
+    {
+        id: 6,
+        name: 'Café frappé',
+        price: 1.35,
+        stock: 50,
+    },
+]
