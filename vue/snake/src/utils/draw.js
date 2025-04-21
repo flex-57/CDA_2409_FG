d505fe394f109ea27e3de6a23b58af9c0bd00631
flex-57 <129852644+flex-57@users.noreply.github.com>
2025-04-21 16:46:58 +0200

    des yeux et basta

---
+++ vue/snake/src/utils/draw.js
@@ -0,0 +1,27 @@
+export const drawCanvas = () => {
+    const drawBg = (ctx, width, height) => {
+        ctx.beginPath()
+        ctx.fillStyle = '#00000050'
+        ctx.textAlign = 'center'
+        ctx.textBaseline = 'middle'
+        ctx.fillRect(0, 0, width, height)
+    }
+
+    const drawText = (ctx, width, heigth, text, color, font) => {
+        ctx.beginPath()
+        ctx.font = font
+        ctx.fillStyle = color
+        ctx.fillText(text, width, heigth)
+    }
+
+    const drawCell = (ctx, x, y, size, color, radius) => {
+        ctx.beginPath()
+        ctx.roundRect(x, y, size, size, [radius])
+        ctx.shadowColor = '#222'
+        ctx.shadowBlur = 12
+        ctx.fillStyle = color
+        ctx.fill()
+    }
+
+    return { drawBg, drawText, drawCell }
+}
